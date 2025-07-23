7ee96418747266e807958820046fc528c86fb599
yanyv-915 <3467286803@qq.com>
2025-07-23 19:27:17 +0800

    不需要的文件

--- assit.cpp
+++
@@ -1,8 +0,0 @@
-#include"assit.h"
-bool isValidNumber(std::string &s){
-    for(char c:s){
-        if(s.empty()) return false;
-        if(!isdigit(c)) return false;
-    }
-    return true;
-}
--- assit.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include<cctype>
-#include<string>
-#include<windows.h>
-bool isValidNumber(std::string &s);
